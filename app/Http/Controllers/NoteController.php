a24b409558608eefdedfb9ef2f741469a4be535a
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-12-12 05:58:07 +0100

    Controller Update

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
@@ -13,7 +14,7 @@ class NoteController extends Controller
      */
     public function index()
     {
-        //
+        return Note::orderBy('created_at', 'DESC');
     }
 
     /**
